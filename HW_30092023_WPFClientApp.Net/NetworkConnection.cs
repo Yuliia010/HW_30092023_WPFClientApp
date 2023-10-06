b64fc8b81df86378c519084f4941d4f463eb8407
Shlyu_hl57@student.itstep.org <Shlyu_hl57@student.itstep.org>
2023-10-06 15:53:35 +0300

    Ex 1 from HW is finished

--- HW_30092023_WPFClientApp.Net/NetworkConnection.cs
+++ HW_30092023_WPFClientApp.Net/NetworkConnection.cs
@@ -8,6 +8,77 @@ namespace HW_30092023_WPFClientApp.Net
 {
     public class NetworkConnection
     {
-      
+        private TcpClient tcpClient;
+        private IPEndPoint endPoint;
+        private NetworkStream stream;
+        private string answer;
+        public async Task Connect(string host, int port)
+        {
+            try
+            {
+                endPoint = new IPEndPoint(IPAddress.Parse(host), port);
+                tcpClient = new TcpClient();
+
+                await tcpClient.ConnectAsync(endPoint);
+            }
+            catch (SocketException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Disconnect()
+        {
+            if (tcpClient != null)
+            {
+                if (tcpClient.Connected)
+                {
+                    tcpClient.Close();
+                    tcpClient.Dispose();
+                }
+            }
+        }
+
+        public bool IsConnected()
+        {
+            if(tcpClient != null)
+            {
+               
+                return tcpClient.Connected;
+            }
+           return false;
+        }
+
+        public async Task SendARequest()
+        {
+            stream = tcpClient.GetStream();
+
+            var requestMessage = $"GET\r\n";
+            var requestData = Encoding.UTF8.GetBytes(requestMessage);
+            await stream.WriteAsync(requestData);
+
+
+        }
+
+        public string ReturnAnswer()
+        {
+            return answer;
+        }
+
+        public async Task GetAnswer()
+        {
+            var responseData = new byte[8192];
+
+            var response = new StringBuilder();
+            int bytes;
+            do
+            {
+                bytes = await stream.ReadAsync(responseData);
+                response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
+            }
+            while (stream.DataAvailable);
+
+            answer = response.ToString();
+        }
     }
 }
--- HW_30092023_WPFClientApp/MainWindow.xaml.cs
+++ HW_30092023_WPFClientApp/MainWindow.xaml.cs
@@ -24,10 +24,99 @@ namespace HW_30092023_WPFClientApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        NetworkConnection connect;
         public MainWindow()
         {
             InitializeComponent();
-            
+            connect = new NetworkConnection();
+        }
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await CheckConnection();
+        }
+        private async Task CheckConnection()
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (connect.IsConnected())
+                        {
+                            btn_Connect.Background = new SolidColorBrush(Colors.LightGreen);
+                            btn_Connect.Content = "Connected";
+                            
+                        }
+                        else
+                        {
+                            btn_Connect.Background = new SolidColorBrush(Colors.IndianRed);
+                            btn_Connect.Content = "Disconnected";
+                        }
+                    });
+
+
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+
+
+        private void btn_GetQuotation_Click(object sender, RoutedEventArgs e)
+        {
+            if(connect.IsConnected())
+            {
+                connect.SendARequest();
+                connect.GetAnswer();
+                tb_Answer.Text = connect.ReturnAnswer();
+            }
+            else
+            {
+                MessageBox.Show("Connect to the server!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+           
+
+        }
+
+
+        private async void btn_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!connect.IsConnected())
+            {
+                if (tb_IP.Text.Length == 0 || tb_Port.Text.Length == 0)
+                {
+                    MessageBox.Show("Input data to connect!");
+                }
+                else
+                {
+                    await ConnectAction();
+                }
+            }
+            else if (connect.IsConnected())
+            {
+                await connect.Disconnect();
+            }
+        }
+
+        private async Task ConnectAction()
+        {
+            string strport = tb_Port.Text;
+            int port;
+            if (int.TryParse(strport, out port))
+            {
+                string host = tb_IP.Text;
+                try
+                {
+                    await connect.Connect(host, port);
+                    MessageBox.Show("Connected to the server");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
     }
