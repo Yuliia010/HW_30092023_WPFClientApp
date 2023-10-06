13883db1c418e7c3b85f49731334da535b1c991d
Shlyu_hl57@student.itstep.org <Shlyu_hl57@student.itstep.org>
2023-10-06 13:03:37 +0300

    Add project files.

---
+++ HW_30092023_WPFClientApp.Net/NetworkConnection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HW_30092023_WPFClientApp.Net
+{
+    public class NetworkConnection
+    {
+      
+    }
+}
